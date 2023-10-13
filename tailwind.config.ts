7a3ab18abcc4035a9a8b45e4ea8dc3ff3a595b7f
Roman <barabanov1233@gmail.com>
2023-10-13 13:53:32 +0400

    Add Salient template

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
 import type { Config } from 'tailwindcss'
+import formsPlugin from '@tailwindcss/forms'
+import headlessuiPlugin from '@headlessui/tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		fontSize: {
+			xs: ['0.75rem', { lineHeight: '1rem' }],
+			sm: ['0.875rem', { lineHeight: '1.5rem' }],
+			base: ['1rem', { lineHeight: '1.75rem' }],
+			lg: ['1.125rem', { lineHeight: '2rem' }],
+			xl: ['1.25rem', { lineHeight: '2rem' }],
+			'2xl': ['1.5rem', { lineHeight: '2rem' }],
+			'3xl': ['2rem', { lineHeight: '2.5rem' }],
+			'4xl': ['2.5rem', { lineHeight: '3.5rem' }],
+			'5xl': ['3rem', { lineHeight: '3.5rem' }],
+			'6xl': ['3.75rem', { lineHeight: '1' }],
+			'7xl': ['4.5rem', { lineHeight: '1.1' }],
+			'8xl': ['6rem', { lineHeight: '1' }],
+			'9xl': ['8rem', { lineHeight: '1' }],
+		},
+		extend: {
+			borderRadius: {
+				'4xl': '2rem',
+			},
+			fontFamily: {
+				sans: 'var(--font-inter)',
+				display: 'var(--font-lexend)',
+			},
+			maxWidth: {
+				'2xl': '40rem',
+			},
+		},
+	},
+	plugins: [formsPlugin, headlessuiPlugin],
 }
 export default config
