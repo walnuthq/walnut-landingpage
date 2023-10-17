b100d266945a4c0fc842d330cf46c957f30ca1b3
Roman <barabanov1233@gmail.com>
2023-10-17 19:07:14 +0400

    Fox logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
+import Image, { type ImageProps } from 'next/image'
 
 import logoWalnut from '@/images/logos/walnut.svg'
 
-export function Logo(props: React.ComponentPropsWithoutRef<'img'>) {
-  return (
-    <Image src={logoWalnut} alt="Walnut logo" unoptimized {...props} />
-  )
+export function Logo({ src, alt, ...props }: Partial<ImageProps>) {
+	return <Image src={src ?? logoWalnut} alt={alt ?? 'Walnut logo'} unoptimized {...props} />
 }
