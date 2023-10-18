5cb4348532a48429b1f6f190e0e57b2668aaaacb
Roman Mazur <roman.mazur@hotmail.com>
2023-10-18 11:58:36 +0200

    Basic layout fixes + logo updates

--- src/components/RequestAccess.tsx
+++ src/components/RequestAccess.tsx
@@ -15,11 +15,11 @@
 export function RequestAccess() {
     return (
         <section id="request-access">
-            <div className=" bg-slate-50 py-16 sm:py-24">
+            <div className=" bg-slate-50 pb-16 sm:pb-24">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
                     <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                    Get notified when Walnut launches.
+                    Get notified when we launch.
                     </h2>
                     <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
                     Sign up now for early access to Walnut Debugger and ascend to new heights in Starknet development.
