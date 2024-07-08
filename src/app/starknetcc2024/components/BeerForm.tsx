b392f03e6ef943022cd916cd81ebf919e2fcaeb6
melnikga <melnik.goshan2013@gmail.com>
2024-07-08 11:49:50 +0300

    Update BeerForm.tsx

--- src/app/starknetcc2024/components/BeerForm.tsx
+++ src/app/starknetcc2024/components/BeerForm.tsx
@@ -132,7 +132,7 @@ export default function BeerForm() {
           (
             <div className="mb-6 md:space-x-10 space-y-2 items-center mx-auto flex justify-center md:flex-row flex-col">
               <Button onClick={handleConnect}>
-                Connect&nbsp;<span className=' block sm:hidden'>{urlRef === 'braavos'? 'Braavos' : 'Argent'}</span>&nbsp;Wallet
+                Connect&nbsp;<span className=' block sm:hidden'>{urlRef === 'braavos'? 'Braavos' : 'Argent'}&nbsp;</span>Wallet
               </Button>
 							{urlRef !== 'braavos' && <Button className=' block sm:hidden' onClick={openBraavosMobile}>Connect Braavos Wallet</Button>}
 							
