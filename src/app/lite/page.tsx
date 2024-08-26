13983545533f27078bca27e59cd77bec6c7c863b
Melnik George <melnik.goshan2013@gmail.com>
2024-08-26 23:11:44 +0300

    New sections

--- src/app/lite/page.tsx
+++ src/app/lite/page.tsx
@@ -4,6 +4,8 @@ import Hero from "@/components/HeroLite";
 import LargeTestimonial from "@/components/large-testimonial";
 import FeaturesPlanet from "@/components/features-planet";
 import Features from "@/components/features-home";
+import CallToAction  from "@/components/lite/CallToAction";
+import Faqs from "@/components/lite/faqs";
 // import TestimonialsCarousel from "@/components/testimonials-carousel";
 // import Cta from "@/components/cta";
 
@@ -13,10 +15,12 @@ export default function Home() {
       <Hero />
       <FeaturesPlanet />
       <Features />
+      <CallToAction/>
       {/* <Features />
       <TestimonialsCarousel />
       <Cta /> */}
       <LargeTestimonial />
+      <Faqs/>
     </>
   );
 }
