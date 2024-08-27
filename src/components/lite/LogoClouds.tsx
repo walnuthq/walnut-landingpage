8f32960b7487a81ab2a776dc61d476a0f3c1960c
Melnik George <melnik.goshan2013@gmail.com>
2024-08-27 15:23:20 +0300

    Some fixes

---
+++ src/components/lite/LogoClouds.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image"
+
+export default function LogoClouds() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+          Backed by Optimism Foundation
+        </h2>
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          <Image
+            alt="Transistor"
+            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          />
+          <Image
+            alt="Reform"
+            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          />
+          <Image
+            alt="Tuple"
+            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          />
+          <Image
+            alt="SavvyCal"
+            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+          />
+          <Image
+            alt="Statamic"
+            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+            width={158}
+            height={48}
+            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
