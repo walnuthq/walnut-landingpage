cc2c91b9cabe53ba4da1f3de417b307464e8884a
marijamijailovic <mijailovicmarija@hotmail.com>
2024-10-31 13:44:39 +0100

    Draf for this week changelog

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -24,25 +24,98 @@ import CodeSnippet20241004 from "@/images/changelog/CodeSnippet20241004.png";
 import CodeSnippet20241011 from "@/images/changelog/CodeSnippet20241011.gif";
 import CodeSnippet20241018 from "@/images/changelog/CodeSnippet20241018.png";
 import CodeSnippet20241025 from "@/images/changelog/CodeSnippet20241025.png";
+import CodeSnippet20241101 from "@/images/changelog/CodeSnippet20241101.png";
 import Link from "next/link";
 
 export default function Page() {
   return (
     <Layout>
-      <Article date={"2024-10-18T00:00Z"} id="step-over">
+      <Article date={"2024-11-01T00:00Z"} id="args-res-simplification">
+        <Image
+          src={CodeSnippet20241101}
+          alt="args-res-simplification"
+          unoptimized
+        />
+        <>
+          <h2 className="font-bold my-4 text-xl">
+            Clearer Arguments and Results Representation
+          </h2>
+          <p className="text-gray-500">
+            This week, we've introduced simplification of argument and result
+            namespaces to streamline call traces and reduce visual complexity.
+            This update helps users to read and understand function calls more
+            easily.
+          </p>
+          <h3 className="flex items-center gap-2 font-bold my-4 text-base">
+            <SparkleIcon className="w-5 h-5" /> More Improvements
+          </h3>
+          <ul className="list-disc text-gray-500 pl-6">
+            <li>
+              Added Debugger Keyboard Shortcuts:
+              <ul className="list-disc list-inside">
+                <li>
+                  <strong>b</strong> - Step Back
+                </li>
+                <li>
+                  <strong>n</strong> - Step Into
+                </li>
+                <li>
+                  <strong>o</strong> - Step Over
+                </li>
+              </ul>
+            </li>
+            <li>
+              Skipped system arguments in the Call Trace to focus on relevant
+              information.
+            </li>
+            <li>
+              Simplified argument names by removing namespaces for easier
+              readability.
+            </li>
+            <li>
+              Improved error messages for incorrect simulation arguments to
+              assist in troubleshooting.
+            </li>
+            <li>
+              Enhanced CASM instruction mappings ensuring that we have fetched
+              all instruction correctly{" "}
+            </li>
+            <li>
+              Provided the chain ID of custom networks to the transaction
+              execution context, required for external execution.
+            </li>
+          </ul>
+        </>
+      </Article>
+
+      <Article date={"2024-10-25T00:00Z"} id="step-over">
         <Image src={CodeSnippet20241025} alt="step-over" unoptimized />
         <>
           <h2 className="font-bold my-4 text-xl">
             Step Over Button in Debugger
           </h2>
           <p className="text-gray-500">
-          We finally added the &apos;Step Over&apos; button, one of the most requested features from our early users. It speeds up the debugging experience by allowing developers to skip over functions and focus on the main logic, saving time and reducing the need to manually step through each line in deeply nested code.
+            We finally added the &apos;Step Over&apos; button, one of the most
+            requested features from our early users. It speeds up the debugging
+            experience by allowing developers to skip over functions and focus
+            on the main logic, saving time and reducing the need to manually
+            step through each line in deeply nested code.
           </p>
           <h3 className="flex items-center gap-2 font-bold my-4 text-base">
             <SparkleIcon className="w-5 h-5" /> More Improvements
           </h3>
           <ul className="list-disc text-gray-500 pl-6">
-            <li>Enhanced the Call Trace with <code>__validate__</code> Account call, to better express <Link href="https://docs.starknet.io/architecture-and-concepts/accounts/account-functions/" className="text-blue-500">Starknet&apos;s Account Abstraction model</Link>.</li>
+            <li>
+              Enhanced the Call Trace with <code>__validate__</code> Account
+              call, to better express{" "}
+              <Link
+                href="https://docs.starknet.io/architecture-and-concepts/accounts/account-functions/"
+                className="text-blue-500"
+              >
+                Starknet&apos;s Account Abstraction model
+              </Link>
+              .
+            </li>
             <li>
               Updated the copy and added a link to the{" "}
               <Link
