d81d5929af4abacd1dbe77d0cebbd253728112f8
marijamijailovic <mijailovicmarija@hotmail.com>
2024-12-05 20:25:35 +0100

    Draft for 06/12/2024 changelog

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -29,11 +29,48 @@ import CodeSnippet20241108 from "@/images/changelog/CodeSnippet20241108.png";
 import CodeSnippet20241115 from "@/images/changelog/CodeSnippet20241115.png";
 import CodeSnippet20241122 from "@/images/changelog/CodeSnippet20241122.png";
 import CodeSnippet20241129 from "@/images/changelog/CodeSnippet20241129.gif";
+import CodeSnippet20241206 from "@/images/changelog/CodeSnippet20241206.png";
 import Link from "next/link";
 
 export default function Page() {
   return (
     <Layout>
+      <Article date={"2024-12-06T00:00Z"} id="execute-function-error-trace">
+        <Image
+          src={CodeSnippet20241206}
+          alt="execute-function-error-trace"
+          unoptimized
+        />
+        <>
+          <h2 className="font-bold my-4 text-xl">Bug Fixes</h2>
+          <p className="text-gray-500">
+            This week, we focused on bug fixing in the call trace. When the
+            <code>__execute__</code> function fails due to the contract not
+            being deployed, the status now correctly shows as{" "}
+            <code>REVERTED</code> with a clear visual error line. Previously,
+            such cases were displayed as successful.
+          </p>
+          <h3 className="flex items-center gap-2 font-bold my-4 text-base">
+            <SparkleIcon className="w-5 h-5" /> More Improvements
+          </h3>
+          <ul className="list-disc text-gray-500 pl-6">
+            <li>
+              Fixed a bug that caused simulation to fail when performed on the
+              latest block number.
+            </li>
+            <li>
+              Resolved an issue where the call trace was not displayed for
+              reverted transactions.
+            </li>
+            <li>
+              Added sierra-replace-ids to the Scarb.toml configuration, ensuring
+              sierra_program_debug_info is included in the contract class during
+              the build process.
+            </li>
+          </ul>
+        </>
+      </Article>
+
       <Article date={"2024-11-29T00:00Z"} id="loop-removal-from-call-trace">
         <Image
           src={CodeSnippet20241129}
@@ -45,18 +82,22 @@ export default function Page() {
             Loop Removal from Call Traces
           </h2>
           <p className="text-gray-500">
-            This week, we streamlined loop handling in the call trace by removing loops and ensuring function calls within them are displayed at the correct level.
+            This week, we streamlined loop handling in the call trace by
+            removing loops and ensuring function calls within them are displayed
+            at the correct level.
           </p>
           <h3 className="flex items-center gap-2 font-bold my-4 text-base">
             <SparkleIcon className="w-5 h-5" /> More Improvements
           </h3>
           <ul className="list-disc text-gray-500 pl-6">
             <li>
-              Adjusted the build configuration during Contract Verification to use the
-              release profile, aligning with Starknet Foundry&apos;s setup.
+              Adjusted the build configuration during Contract Verification to
+              use the release profile, aligning with Starknet Foundry&apos;s
+              setup.
             </li>
             <li>
-              Fixed an issue in Transaction Simulations where errors weren’t shown for undeployed contracts.
+              Fixed an issue in Transaction Simulations where errors weren’t
+              shown for undeployed contracts.
             </li>
             <li>
               Updated the documentation{" "}
@@ -66,11 +107,12 @@ export default function Page() {
               >
                 Walnut Verification Guide
               </Link>{" "}
-              with the new Walnut API Contract Verification service, ensuring users are
-              up to date with the latest functionality.
+              with the new Walnut API Contract Verification service, ensuring
+              users are up to date with the latest functionality.
             </li>
             <li>
-              Improved frontend performance by removing regexes from Call Trace function formatting.
+              Improved frontend performance by removing regexes from Call Trace
+              function formatting.
             </li>
           </ul>
         </>
