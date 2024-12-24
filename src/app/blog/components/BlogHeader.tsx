5a8130ddf0948658c7e19b4468a95f5e2557c563
mobycrypt <pt@mobycrypt.com>
2024-12-24 10:38:41 +0100

    Fixes

---
+++ src/app/blog/components/BlogHeader.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+import { Header } from '@/components/Header'
+
+import backgroundImage from '@/images/background-blog.png'
+
+export default function BlogHeader() {
+    return <>
+        <div className="absolute z-20 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none">
+            <div className="w-[108rem] flex-none flex justify-end">
+                <Image
+                    className="w-[71.75rem] flex-none max-w-none"
+                    src={backgroundImage}
+                    alt=""
+                    width={1558}
+                    height={946}
+                    unoptimized
+                />
+            </div>
+        </div>
+        <div className='z-40 backdrop-blur bg-white/60'>
+            <Header condensed />
+        </div>
+    </>
+}
