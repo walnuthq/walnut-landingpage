b42c9f90e703c6d15d88ed461ccdd5d44c5be058
mobycrypt <pt@mobycrypt.com>
2024-12-24 12:44:54 +0100

    Fixes

--- src/app/components/logo-cloud.tsx
+++ src/app/components/logo-cloud.tsx
@@ -21,22 +21,22 @@ export function LogoCloud({
         />
         <img
           alt="Starkware"
-          src="/logo-cloud/StarkWare Logo.png"
+          src="/logo-cloud/StarkWare_Logo.png"
           className="h-9 max-sm:mx-auto sm:h-6 lg:h-8 self-center"
         />
         <img
           alt="Argent Wallet Logo"
-          src="/logo-cloud/Argent Wallet Logo.png"
+          src="/logo-cloud/Argent_Wallet_Logo.png"
           className="h-9 max-sm:mx-auto sm:h-8 lg:h-16"
         />
         <img
           alt="Dojo"
-          src="/logo-cloud/Dojo Logo Starknet.png"
+          src="/logo-cloud/Dojo_Logo_Starknet.png"
           className="h-9 max-sm:mx-auto sm:h-8 lg:h-16"
         />
         <img
           alt="CF"
-          src="/logo-cloud/Vesu Logo.png"
+          src="/logo-cloud/Vesu_Logo.png"
           className="h-9 max-sm:mx-auto sm:h-4 lg:h-6 self-center"
         />
       </div>
