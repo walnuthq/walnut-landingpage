50d5eee0a70d04a1c3e84c00f5369c4c4a6898e4
mobycrypt <pt@mobycrypt.com>
2025-01-03 17:28:44 +0100

    Fixes in Dojo logo

--- src/app/components/logo-cloud.tsx
+++ src/app/components/logo-cloud.tsx
@@ -4,8 +4,8 @@ export function LogoCloud({
   className,
 }: React.ComponentPropsWithoutRef<'div'>) {
   return (
-    <div className='flex  md:flex-row flex-col justify-between items-center'>
-      <div className='text-gray-500 mb-8 md:mb-0'>
+    <div className='flex md:flex-row flex-col justify-between items-center'>
+      <div className='text-gray-500 mb-6 md:mb-0'>
         Trusted by the best.
       </div>
       <div
@@ -17,27 +17,27 @@ export function LogoCloud({
         <img
           alt="StarknetLogo"
           src="/logo-cloud/Starknet_Logo.svg"
-          className="h-9 max-sm:mx-auto sm:h-6 lg:h-10 self-center"
+          className="max-sm:mx-auto h-7 lg:h-9 self-center"
         />
         <img
           alt="Starkware"
           src="/logo-cloud/StarkWare_Logo.png"
-          className="h-9 max-sm:mx-auto sm:h-6 lg:h-8 self-center"
+          className="max-sm:mx-auto h-6 lg:h-8 self-center"
         />
         <img
           alt="Argent Wallet Logo"
           src="/logo-cloud/Argent_Wallet_Logo.png"
-          className="h-9 max-sm:mx-auto sm:h-8 lg:h-16"
+          className="max-sm:mx-auto h-12 lg:h-14 self-center"
         />
         <img
           alt="Dojo"
           src="/logo-cloud/Dojo_Logo_Starknet.png"
-          className="h-9 max-sm:mx-auto sm:h-8 lg:h-16"
+          className="max-sm:mx-auto h-10 lg:h-14"
         />
         <img
           alt="CF"
           src="/logo-cloud/Vesu_Logo.png"
-          className="h-9 max-sm:mx-auto sm:h-4 lg:h-6 self-center"
+          className="max-sm:mx-auto h-4 lg:h-6 self-center"
         />
       </div>
     </div>
