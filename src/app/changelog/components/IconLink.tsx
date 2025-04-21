dc2d19bf76f7bebbd3aa02928993db3d4287d964
Melnik George <melnik.goshan2013@gmail.com>
2025-04-21 18:03:28 +0300

    The new footer

--- src/app/changelog/components/IconLink.tsx
+++ src/app/changelog/components/IconLink.tsx
@@ -26,7 +26,7 @@ export function IconLink({
       )}
     >
       <span className={`absolute inset-0 -z-10 scale-75 rounded-lg bg-white/5 opacity-0 transition ${!isFooter && 'group-hover:scale-100 group-hover:opacity-100'}`} />
-      {Icon && <Icon className="h-4 w-4 flex-none" />}
+      {Icon && <Icon className="h-5 w-5 flex-none text-GREY-2 hover:text-BLUE transition-all" />}
       <span className={`self-baseline text-white`}>{children}</span>
     </Link>
   )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,31 @@
 import Link from 'next/link'
 
-import { Container } from '@/components/Container'
-import { Logo } from '@/components/Logo'
-import { NavLink } from '@/components/NavLink'
 import { IconLink } from '@/app/changelog/components/IconLink'
 import { GitHubIcon, TelegramIcon, XIcon } from '@/app/changelog/components/Intro'
 
+
 export function Footer() {
   return (
-    <footer className="bg-slate-50">
-      <Container>
-        <div className="flex flex-col items-center border-t border-slate-400/10 py-4 sm:flex-row-reverse sm:justify-between">
-        <div className='flex gap-x-1'>
-          <IconLink isFooter href="https://x.com/walnut_dev" target="_blank" icon={XIcon} variant='black'>
+    <footer className="bg-GREY">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+        <nav aria-label="Footer" className="-mb-6 flex flex-wrap justify-center gap-x-12 gap-y-3 text-sm/6">
+          <Link className="text-GREY-2 hover:text-BLUE" target='_blank' href="https://docs.walnut.dev">Docs</Link>
+          <Link className="text-GREY-2 hover:text-BLUE" href="/blog">Blog</Link>
+          <Link className="text-GREY-2 hover:text-BLUE" href="/changelog-new">Changelog</Link>
+          <Link className="text-GREY-2 hover:text-BLUE" href="/">Hiring</Link>
+          <Link className="text-GREY-2 hover:text-BLUE" href="mailto:hi@walnut.dev">Contact</Link>
+        </nav>
+        <div className="mt-16 flex justify-center gap-x-10">
+        <IconLink isFooter href="https://x.com/walnut_dev" target="_blank" icon={XIcon} variant='black'>
           </IconLink>
           <IconLink isFooter href="https://github.com/walnuthq" target="_blank" icon={GitHubIcon} variant='black'>
             </IconLink>
           <IconLink isFooter href="https://t.me/walnuthq" target="_blank" icon={TelegramIcon} variant='black'>
           </IconLink>
         </div>
-          <p className="mt-6  text-[0.75rem] text-slate-500 sm:mt-0">
-            Copyright &copy; {new Date().getFullYear()} Wido Inc. All rights
-            reserved.
-          </p>
-        </div>
-      </Container>
+        <p className="mt-10 text-center text-sm/6 text-GREY-2">Copyright &copy; {new Date().getFullYear()} Wido Inc. All rights
+        reserved.</p>
+      </div>
     </footer>
   )
 }
