6ccd085d19085bd1fcc13bb718ea12353d8f7fc6
Melnik George <melnik.goshan2013@gmail.com>
2025-05-02 17:29:38 +0300

    Added 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,27 +1,27 @@
-import Link from 'next/link'
-
-import { Button } from '@/components/Button'
-import { Logo } from '@/components/Logo'
-import { SlimLayout } from '@/components/SlimLayout'
+import { Button } from "./components/button";
 
 export default function NotFound() {
   return (
-    <SlimLayout>
-      <div className="flex">
-        <Link href="/" aria-label="Home">
-          <Logo className="h-10 w-auto" />
-        </Link>
-      </div>
-      <p className="mt-20 text-sm font-medium text-gray-700">404</p>
-      <h1 className="mt-3 text-lg font-semibold text-gray-900">
-        Page not found
-      </h1>
-      <p className="mt-3 text-sm text-gray-700">
-        Sorry, we couldn’t find the page you’re looking for.
-      </p>
-      <Button href="/" className="mt-10">
-        Go back home
-      </Button>
-    </SlimLayout>
+    <>
+      <main className="grid min-h-screen place-items-center bg-WHITE px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-BLUE">404</p>
+          <h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+            Page not found
+          </h1>
+          <p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Button
+              href="/"
+              className="bg-BLUE px-3.5 py-2.5 text-sm font-semibold text-WHITE shadow-sm"
+            >
+              Go back home
+            </Button>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
