969d0bfa7595bc16d9f8b67dccb201df2b10a925
Melnik George <melnik.goshan2013@gmail.com>
2025-05-09 15:03:35 +0300

    Fixes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -21,60 +21,67 @@ export const metadata: Metadata = generateMetadata(title, description, 'https://
 
 export default async function Contact() {
   return (
-    <main className="overflow-hidden relative">
+<main className="overflow-hidden relative">
       <MainHeader/>
-      <Container className="pt-16 lg:pt-36 static  min-h-screen ">
-        <div className="relative isolate lg:bg-WHITE">
-        <div className="mx-auto flex justify-end max-w-7xl grid-cols-1 lg:grid-cols-2">
-          <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
-            <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-              <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden lg:w-1/2 hidden lg:block">
-                <Image alt='bg' src={contact_pattern} className='w-full h-full'/>
+      <Container className="min-h-screen">
+        <div className="relative isolate lg:bg-WHITE w-full flex items-center min-h-screen">
+          <div className="mx-auto max-w-7xl w-full grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+            <div className="relative hidden lg:block">
+              <div className="w-full">
+                <Image 
+                  alt='contact image' 
+                  src={contact_pattern} 
+                  className='w-full' 
+                  priority
+                />
               </div>
-              <Heading as="h2" className="text-3xl/[1.2] font-medium tracking-tight text-BLACK sm:text-5xl">
-                Get in touch
-              </Heading>
-              <p className="mt-6 text-lg/8 text-GREY-2">
-                Whether you have a questiona project in mind or just want to say hi, we&apos;d love to hear from you.
-              </p>
-              <dl className="mt-10 space-y-4 text-base/7 text-GREY-2">
-                <div className="flex items-center gap-x-4">
-                  <dt className="flex-none">
-                    <span className="sr-only">Email</span>
-                    <Image alt='email_logo' src={contact_logo_email} className='w-6 opacity-50'/>
-                  </dt>
-                  <dd>
-                  <a href="mailto:hi@walnut.dev" className="hover:text-BLACK" target='_blank'>
-                      hi@walnut.dev
-                    </a>
-                  </dd>
-                </div>
-                <div className="flex items-center gap-x-4">
-                  <dt className="flex-none">
-                    <span className="sr-only">Telegram</span>
-                    <Image alt='tg_logo' src={contact_logo_tg} className='w-6 opacity-50'/>
-                  </dt>
-                  <dd>
-                    <a href="https://t.me/walnuthq" className="hover:text-BLACK" target='_blank'>
-                      walnuthq
-                    </a>
-                  </dd>
-                </div>
-                <div className="flex items-center gap-x-4">
-                  <dt className="flex-none">
-                    <span className="sr-only">X</span>
-                    <Image alt='x_logo' src={contact_logo_x} className='w-6 opacity-50'/>
-                  </dt>
-                  <dd>
-                    <a href="https://x.com/walnut_dev" className="hover:text-BLACK" target='_blank'>
-                      @walnut_dev
-                    </a>
-                  </dd>
-                </div>
-              </dl>
+            </div>
+            <div className="relative px-6 lg:static lg:px-8">
+              <div className="max-w-xl lg:max-w-lg">
+                <Heading as="h2" className="text-3xl/[1.2] font-medium tracking-tight text-BLACK sm:text-5xl">
+                  Get in touch
+                </Heading>
+                <p className="mt-6 text-lg/8 text-GREY-2">
+                  Whether you have a questiona project in mind or just want to say hi, we&apos;d love to hear from you.
+                </p>
+                <dl className="mt-10 space-y-4 text-base/7 text-GREY-2">
+                  <div className="flex items-center gap-x-4">
+                    <dt className="flex-none">
+                      <span className="sr-only">Email</span>
+                      <Image alt='email_logo' src={contact_logo_email} className='w-6 opacity-50'/>
+                    </dt>
+                    <dd>
+                      <a href="mailto:hi@walnut.dev" className="hover:text-BLACK" target='_blank'>
+                        hi@walnut.dev
+                      </a>
+                    </dd>
+                  </div>
+                  <div className="flex items-center gap-x-4">
+                    <dt className="flex-none">
+                      <span className="sr-only">Telegram</span>
+                      <Image alt='tg_logo' src={contact_logo_tg} className='w-6 opacity-50'/>
+                    </dt>
+                    <dd>
+                      <a href="https://t.me/walnuthq" className="hover:text-BLACK" target='_blank'>
+                        walnuthq
+                      </a>
+                    </dd>
+                  </div>
+                  <div className="flex items-center gap-x-4">
+                    <dt className="flex-none">
+                      <span className="sr-only">X</span>
+                      <Image alt='x_logo' src={contact_logo_x} className='w-6 opacity-50'/>
+                    </dt>
+                    <dd>
+                      <a href="https://x.com/walnut_dev" className="hover:text-BLACK" target='_blank'>
+                        @walnut_dev
+                      </a>
+                    </dd>
+                  </div>
+                </dl>
+              </div>
             </div>
           </div>
-        </div>
         </div>
       </Container>
       <GetStarted />
