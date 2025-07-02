d3462ff0660ef89aca19c4201d1e9fbe05446c2b
Roman Mazur <roman.mazur@hotmail.com>
2025-07-02 13:58:21 +0200

    Copy

--- src/app/blog/gas-calculation/page.tsx
+++ src/app/blog/gas-calculation/page.tsx
@@ -67,7 +67,8 @@ export default function Blog() {
                   <main>
                     <article className="relative">
                       <div className="mt-12 mb-24 prose">
-                        <p className="lead">If you&apos;ve ever looked at a Starknet transaction and wondered where all the gas goes — we’ve got the answer.
+                        <p className="lead">
+                        Ever looked at a Starknet transaction and wondered why it cost what it did? The answer lies in how gas is calculated across L2 execution, L1 data and L1 gas usage. In this post, we break down the L2 gas component — and introduce a new Flamegraph tool in Walnut that lets you visualize exactly where your transaction spent gas, down to each contract and Cairo function call.
                         </p>
                         <p>Every Starknet transaction incurs fees that come from three different gas components:</p>
                         <ul>
@@ -75,11 +76,11 @@ export default function Blog() {
                             <strong>L2 gas</strong> – measures computation, code complexity and data usage on Starknet (L2).
                           </li>
                           <li>
-                            <strong>L1 data gas</strong> - covers the cost of posting the transaction’s state diff 
+                            <strong>L1 data gas</strong> – covers the cost of posting the transaction’s state diff 
                             (e.g., storage changes or class declarations) to L1 for data availability.
                           </li>
                           <li>
-                            <strong>L1 gas fee </strong> - applies when sending messages from L2 to L1, or when calldata (instead of blobs) is used for data availability.
+                            <strong>L1 gas fee </strong> – applies when sending messages from L2 to L1, or when calldata (instead of blobs) is used for data availability.
                           </li>
                         </ul>
                         <p>
@@ -90,13 +91,13 @@ export default function Blog() {
                           For more details, see the <a href="https://docs.starknet.io/architecture/fees/#fee_formula" target="_blank">
                           Starknet fee mechanism documentation.</a>
                         </p>
-                        <p>In this post we will break down into how L2 gas is calculated on Starknet, using a real transaction to illustrate how 
+                        <p>In this post we will break down how L2 gas is calculated on Starknet, using a real transaction to illustrate how 
                           execution and data resources contribute to cost. We also introduce a brand new Flamegraph visualisation tool in Walnut 
                           that helps Cairo developers analyze Starknet transaction costs on a granular level – making it easy to see exactly where 
                           gas is spent.
                         </p>
                         <h2>
-                          Example: analysing gas cost of a sample transaction
+                          Analysing L2 gas cost of a sample transaction using Walnut Call Trace with Gas Information
                         </h2>
                         <p>
                           We’ll use the following transaction {' '}
@@ -122,79 +123,92 @@ export default function Blog() {
                             </div>
                           </div>
                           <figcaption>
-                          🔎  Click each &quot;blue gas&quot; chip on the right to get quick access to a specific level of the Flamegraph.
+                          🔎 Tip: Click each &quot;blue gas&quot; chip on the right to get quick access to a specific level of the Flamegraph.
                           </figcaption>
                         </figure>
                         <p>
-                          You can see the transaction in the image above. It involves a few contract calls, each with its own gas cost visualised in the blue chip on the right:
+                          The image above shows a full Call Trace on Walnut with enhanced Gas Information. It involves a few contract calls, each with its own gas cost visualised in the blue chip on the right:
                         </p>
                         <ul>
                           <li>
-                            <span className="text-BLUE font-mono">AccountContract</span>.<span className="text-red-600 p-1 bg-GREY rounded-sm font-mono">__validate__</span>: 86,635
+                            <span className="text-BLUE font-mono">AccountContract</span>.<span className="text-red-600 p-1 bg-GREY rounded-sm font-mono">__validate__</span>: 86,635 L2 Gas
                           </li>
                           <li>
-                            <span className="text-BLUE font-mono">AccountContract</span>.<span className="text-red-600 font-mono p-1 bg-GREY rounded-sm">__execute__</span>: 27,435,820
+                            <span className="text-BLUE font-mono">AccountContract</span>.<span className="text-red-600 font-mono p-1 bg-GREY rounded-sm">__execute__</span>: 27,435,820 L2 Gas
                           </li>
                           <li className="break-words">
                             <span className="text-BLUE font-mono">0x7b14d2b859efbbc97d68ca769d6e9b6a1c1fd9629d213e8555335448dc759bf</span>.
-                            <span className="text-red-600 font-mono p-1 bg-GREY rounded-sm">test_call_contract</span>: 27,710,801
+                            <span className="text-red-600 font-mono p-1 bg-GREY rounded-sm">test_call_contract</span>: 27,710,801 L2 Gas
                           </li>
                           <li className="break-words">
                             <span className="text-BLUE font-mono">0x69b0b8db5469b82390505d02cda4ca08bebf50ca0d824a88b6756fcd5aaf5e5</span>.
-                            <span className="text-red-600 font-mono p-1 bg-GREY rounded-sm">test_signature_verification_secp256r1</span>: 27,355,850
+                            <span className="text-red-600 font-mono p-1 bg-GREY rounded-sm">test_signature_verification_secp256r1</span>: 27,355,850 L2 Gas
                           </li>
                           <li className="break-words">
                             <span className="text-BLUE font-mono">0x7b14d2b859efbbc97d68ca769d6e9b6a1c1fd9629d213e8555335448dc759bf</span>.
-                            <span className="text-red-600 font-mono p-1 bg-GREY rounded-sm">test_poseidon_hades_permutation</span>: 6,131
+                            <span className="text-red-600 font-mono p-1 bg-GREY rounded-sm">test_poseidon_hades_permutation</span>: 6,131 L2 Gas
                           </li>
                         </ul>
-                        <div className={`lg:bg-GREY lg:p-2`}>
-                          <div 
-                            className={`relative overflow-hidden [&+*]:mt-8 lg:bg-[url('/changelog-bg.svg')] bg-cover bg-center no-repeat lg:p-4`}
-                          >
-                            <Image src={flamechart} alt="" className='lg:shadow-2xl rounded-3xl w-full h-auto'/>
-                          </div>
-                        </div>
                         <p>
-                          The image above presents the gas Flamechart for our transaction. 
-                          The root frame is the total L2 Gas of the transaction and it is divided into two main parts:
+                        This detailed breakdown of gas usage helps you understand, on a granular level, the gas cost per contract and function calls in your Starknet transactions.
                         </p>
 
-
+                        <h2>Introducing Flamecharts for Cairo Transactions</h2>
+                        <p>
+                          To provide a more visual representation of where the majority of gas is spent, we are introducing Flamecharts for Cairo transactions.
+                          They are a great way to understand the gas usage of a transaction and to identify the most expensive parts of the transaction.
+                        </p>
+                        <figure>
+                          <div className={`lg:bg-GREY lg:p-2`}>
+                            <div 
+                              className={`relative overflow-hidden [&+*]:mt-8 lg:bg-[url('/changelog-bg.svg')] bg-cover bg-center no-repeat lg:p-4`}
+                            >
+                              <Image src={flamechart} alt="" className='lg:shadow-2xl rounded-3xl w-full h-auto'/>
+                            </div>
+                          </div>
+                          <figcaption>
+                            Flamechart for our transaction clearly reveals that the majority of L2 gas is spent on <code>test_signature_verification_secp256r1</code> function call.
+                          </figcaption>
+                        </figure>
+                        <p>
+                          The image above presents the gas Flamechart for our transaction. 
+                          The root frame represents the total L2 Gas of the transaction and it is then divided into two main parts:
+                        </p>
                         <ul>
                           <li>
-                            <span className="font-bold">Computation resources:</span> actual Cairo (or more specifically Sierra) execution:
-                          </li>
-                          <ul>
-                            <li>VM cost: Measures how many VM steps were executed together with the usage of builtins e.g. 
-                              <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">range_check</span>, <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">pedersen</span>, <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">poseidon</span>, etc.
-                            </li>
-                            <li>
-                            Sierra cost: it tracks the logical structure and complexity of the code, summing the cost of each Sierra libfunc 
-                            (libfuncs are similar to opcodes in the EVM, e.g. <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">u8_add </span> is a libfunc).
-                            </li>
-                          </ul>
-                          <li className="list-none">
-                            <p>Each frame under Computation resources represents a contract call execution and its children — internal calls made by that contract.</p>
+                            <span className="font-bold">Computation Resources:</span> Actual Cairo (or more specifically Sierra) execution cost. In most cases, this is where the majority of L2 gas goes and where optimization efforts should be focused on. Computation resources are further divided into two sub-components:
+                            <ul>
+                              <li><span className="font-bold">VM cost:</span> Measures how many VM steps were executed together with the usage of builtins e.g. 
+                                <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">range_check</span>, <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">pedersen</span>, <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">poseidon</span>, etc.
+                              </li>
+                              <li><span className="font-bold">Sierra cost:</span> Tracks the logical structure and complexity of the code. The gas cost is calculated by summing the cost of each Sierra libfunc 
+                              (libfuncs are similar to opcodes in the EVM, e.g. <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">u8_add</span> is a libfunc).
+                                <ul>
+                                  <li>
+                                      Each frame under <code>AccountContract.__execute__</code> represents a contract call execution and its children — internal calls made by that contract.
+                                  </li>
+                                </ul>
+                              </li>
+                            </ul>
                           </li>
                         </ul>
                         <ul>
                           <li>
-                            <span className="font-bold">Starknet resources:</span> system-level overhead:
+                            <span className="font-bold">Starknet resources:</span> Starknet system-level overhead:
                           </li>
                           <ul>
                             <li>
-                              Archival resources: cost of storing calldata, signatures, declared class code, and emitted events on L2.
+                              <span className="font-bold">Archival resources:</span> Cost of storing calldata, signatures, declared class code, and emitted events on L2.
                             </li>
                             <li>
-                              Messages resources: gas usage for processing L1 ↔ L2 messages (e.g., <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">send_message_to_l1</span>).
+                              <span className="font-bold">Messages resources:</span> Gas usage for processing L1 ↔ L2 messages (e.g., <span className="font-mono p-1 bg-GREY rounded-sm text-red-600">send_message_to_l1</span>).
                             </li>
                             <li>
-                            State resources: cost of storage writes, class hash updates, compiled class updates, and contract deployments. Also, number of new storage keys allocated in the transaction.
+                              <span className="font-bold">State resources:</span> Cost of storage writes, class hash updates, compiled class updates, and contract deployments. Also, number of new storage keys allocated in the transaction.
                             </li>
                           </ul>
                         </ul>
-                        <p>This structure is what the Flamegraph visualizes, helping you trace gas usage from top-level categories to specific calls.</p>
+                        <p>The below structure is a visual representation of all the key resources the Flamegraph shows, helping you trace gas usage from top-level categories to specific calls.</p>
                         <pre className="font-mono text-sm bg-gray-800 text-gray-300 p-4 rounded-lg overflow-x-auto">
                           {asciiTree.split('\n').map((line, idx) => {
                             return (
@@ -204,7 +218,7 @@ export default function Blog() {
                             );
                           })}
                         </pre>
-                        <h3>Cheat Sheet for Optimizing Cairo Transactions Gas Usage</h3>
+                        <h2>Cheat Sheet for Optimizing Cairo Transactions Gas Usage</h2>
                         <p>Once you understand which part of the transaction consumes what, you can target your optimizations more effectively. 
                           Here’s a quick cheat sheet on what to watch out for, and how to reduce the impact of each component:</p>
                           <div className="w-full overflow-x-auto">
@@ -245,7 +259,7 @@ export default function Blog() {
                           </p>
                           <p>In the upcoming posts, we&apos;ll dive deeper into each gas component with hands-on tips for optimizing your contracts more effectively.</p>
                         <h3>Summary</h3>
-                        <p>In this post, we focused on one important part of Starknet gas - the L2 gas. We explained different components 
+                        <p>In this post, we focused on one important part of Starknet Transaction Gas Analysis—the L2 gas. We explained different components 
                           of it and presented Walnut’s new feature that helps you analyse how your Starknet Application consumes those 
                           resources on a granular level.
                         </p>
