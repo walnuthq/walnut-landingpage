2be448552c1bbe858709cc2ad1e698aaa3cb7685
Melnik George <melnik.goshan2013@gmail.com>
2025-07-07 16:38:29 +0300

    Update BlogScreenshot.tsx

--- src/app/blog/components/BlogScreenshot.tsx
+++ src/app/blog/components/BlogScreenshot.tsx
@@ -7,7 +7,9 @@ interface FigureProps {
   caption?: ReactNode | string;
 }
 
-const Figure: React.FC<FigureProps> = ({ src, alt = '', caption }) => {
+const BlogScreenshot: React.FC<FigureProps> = ({ src, alt = '', caption }) => {
+  const href = typeof src === 'string' ? src : src.src;
+
   const renderCaption = () => {
     if (!caption) return null;
     if (typeof caption === 'string' && /<[^>]+>/.test(caption)) {
@@ -20,21 +22,27 @@ const Figure: React.FC<FigureProps> = ({ src, alt = '', caption }) => {
     <figure>
       <div className="lg:bg-GREY lg:p-2 lg:-mx-24">
         <div className="relative overflow-hidden [&+*]:mt-8 lg:bg-[url('/changelog-bg.svg')] bg-cover bg-center no-repeat lg:px-[5.5rem]">
-          <Image
-            src={src}
-            alt={alt? alt : ''}
-            className="lg:shadow-2xl lg:block hidden rounded-3xl w-full h-auto max-w-2xl md:max-w-7xl mx-auto shadow-[0_0_20px_rgba(0,0,0,0.25)]"
-          />
+          <a href={href} target="_blank" rel="noopener noreferrer" className="block">
+            <Image
+              src={src}
+              alt={alt}
+              className="lg:shadow-2xl lg:block hidden rounded-3xl w-full h-auto max-w-2xl md:max-w-7xl mx-auto shadow-[0_0_20px_rgba(0,0,0,0.25)]"
+            />
+          </a>
         </div>
       </div>
-      <Image
-        src={src}
-        alt={alt? alt : ''}
-        className="lg:shadow-2xl lg:hidden rounded-3xl w-full h-auto max-w-2xl md:max-w-7xl mx-auto shadow-[0_0_20px_rgba(0,0,0,0.25)]"
-      />
-      {caption && renderCaption()}
+
+      <a href={href} target="_blank" rel="noopener noreferrer" className="block lg:hidden">
+        <Image
+          src={src}
+          alt={alt}
+          className="rounded-3xl w-full h-auto max-w-2xl md:max-w-7xl mx-auto shadow-[0_0_20px_rgba(0,0,0,0.25)]"
+        />
+      </a>
+
+      {renderCaption()}
     </figure>
   );
 };
 
-export default Figure;
+export default BlogScreenshot;
