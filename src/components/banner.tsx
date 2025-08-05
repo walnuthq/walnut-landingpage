16bfe1c20c5bb1f8f48b8895153c8b04205b6566
Melnik George <91109124+melnikga@users.noreply.github.com>
2025-08-06 00:51:41 +0400

    Fixed mobile banner (#148)
    * Update banner.tsx
    
    * Fixes for small screens

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,17 +1,17 @@
-
-
 export default function Banner() {
   return (
-    <div className="flex justify-center items-center gap-x-6 bg-BLUE lg:px-6 py-1 sm:py-2.5 sm:px-3.5 mt-14 absolute left-0 right-0 top-6 md:top-4 z-30">
-      <p className="text-sm/6 text-WHITE">
-        <a href="https://github.com/walnuthq/walnut" target='_blank'>
+    <div className="flex justify-center items-center gap-x-6 bg-BLUE lg:px-6 py-2 sm:py-2.5 px-3 sm:px-3.5 mt-14 absolute left-0 right-0 top-6 md:top-4 z-30">
+      <p className="text-sm compact-banner text-WHITE text-center leading-relaxed">
+        <a href="https://github.com/walnuthq/walnut" target='_blank' className="block sm:inline">
           <strong className="font-semibold">Introducing Walnut EVM</strong>
-          <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2  size-0.5 fill-current hidden lg:inline">
+          <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 size-0.5 fill-current hidden lg:inline">
             <circle r={1} cx={1} cy={1} />
           </svg>
-          <span className='hidden lg:inline'>An Open-Source Transaction Debugger and Simulator for Ethereum and its Rollups
-          </span>
+          <span className='block sm:inline mt-1 sm:mt-0'>
+            An Open-Source Transaction Debugger and Simulator for Ethereum and its Rollups
+
           <span aria-hidden="true" className='ml-2'>&rarr;</span>
+          </span>
         </a>
       </p>
     </div>
