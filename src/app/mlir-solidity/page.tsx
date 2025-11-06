14d1be2d5ed51f7285c1dd29aff01e40d269f42e
Melnik George <melnik.goshan2013@gmail.com>
2025-11-06 04:03:20 +0300

    Update page.tsx

--- src/app/mlir-solidity/page.tsx
+++ src/app/mlir-solidity/page.tsx
@@ -50,7 +50,7 @@ export default function MLIR() {
             </Lead>
             <div className="flex items-center justify-center gap-3 text-sm">
               <div className="flex items-center gap-2 bg-white px-4 py-2 rounded-full shadow-sm border border-slate-200">
-                <Image src={ethLogo} alt="eth" className="w-5 h-5"/>
+                {/* <Image src={ethLogo} alt="eth" className="w-5 h-5"/> */}
                 <span className="font-medium text-BLACK">Supported by the Ethereum Foundation</span>
               </div>
             </div>
